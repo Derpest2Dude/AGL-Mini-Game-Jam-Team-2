1f4ee0d0d18de4e720a1e9fea360130c341f7201
Gooeys1 <jason.shervanick@gmail.com>
2026-09-24 15:48:27 -0700

    Adjust movement button checks

--- Scenes/Test Scenes/character_body_3d.gd
+++ Scenes/Test Scenes/character_body_3d.gd
@@ -37,7 +37,7 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_just_pressed("move_right"):
 		direction = 1
 	if is_on_floor():
-		if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+		if (Input.is_action_pressed("move_left") and direction == -1) or (Input.is_action_pressed("move_right") and direction == 1):
 			if velocity.normalized().x == direction:
 				velocity.x = move_toward(velocity.x, MOVE_SPEED * direction, MOVE_ACCEL)
 			elif velocity.normalized().x != direction:
